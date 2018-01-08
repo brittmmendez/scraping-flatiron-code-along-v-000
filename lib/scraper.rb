9ff8af2445801e6b6927b047254a68e66c60d340
Brittany Morris <brittmmendez@gmail.com>
2018-01-08 19:28:51 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,9 +6,8 @@ require_relative './course.rb'
 
 class Scraper
 
-    profile_url="dp/B0192CTMYG/ref=chrt_bk_rd_fc_3_ci_lp"
-    url="https://www.amazon.com/#{profile_url}"
-    doc = Nokogiri::HTML(open("https://www.amazon.com/charts"))
+    url="https://www.amazon.com/charts"
+    doc = Nokogiri::HTML(open(url))
     book=doc.css(".kc-horizontal-rank-card"))
 
     binding.pry
