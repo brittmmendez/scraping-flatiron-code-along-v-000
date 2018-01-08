38578486a3594e127346d9e2efffc00e489cebe7
Brittany Morris <brittmmendez@gmail.com>
2018-01-08 19:28:11 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,32 +5,17 @@ require 'pry'
 require_relative './course.rb'
 
 class Scraper
-  def get_page
-      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-  end
 
-  def get_courses
-    self.get_page.css(".post")
-  end
+    profile_url="dp/B0192CTMYG/ref=chrt_bk_rd_fc_3_ci_lp"
+    url="https://www.amazon.com/#{profile_url}"
+    doc = Nokogiri::HTML(open("https://www.amazon.com/charts"))
+    book=doc.css(".kc-horizontal-rank-card"))
 
-  def make_courses
-    self.get_courses.each do |post|
-    course = Course.new
-    course.title = post.css("h2").text
-    course.schedule = post.css(".date").text
-    course.description = post.css("p").text
-    end
-  end
+    binding.pry
 
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
 end
 Scraper.new.print_courses
+
+    #profile_url="dp/B0192CTMYG/ref=chrt_bk_rd_fc_3_ci_lp"
+    #url="https://www.amazon.com/#{profile_url}"
+    #doc = Nokogiri::HTML(open(url))
